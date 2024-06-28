fde1f8b3e3dd6c85448c6967e9d21dd7febe5214
chocoden <kazuma.sf.soccer@gmail.com>
2024-06-28 18:19:50 +0900

    一回目

--- database/migrations/2024_06_27_130747_create_reviews_table.php
+++ database/migrations/2024_06_27_130747_create_reviews_table.php
@@ -11,14 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('reviews', function (Blueprint $table) {
             $table->id();
-            $table->string('username');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->foreignID('toilet_id')->constrained()->cascadeOnDelete();
+            $table->foreignID('user_id')->constrained()->cascadeOnDelete();
+            $table->double('rating',2,1);
+            $table->string('comment');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('reviews');
     }
 };
